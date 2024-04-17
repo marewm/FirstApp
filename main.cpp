41db747e5edc246c47a14c6e233805763de6882e
Mariia K <mkgordenko@gmail.com>
2024-04-17 16:56:19 +0300

    some comment

--- main.cpp
+++ main.cpp
@@ -2,9 +2,18 @@
 // Created by Mariia Gordenko on 17.04.2024.
 //
 #include <iostream>
+#include <string>
 
 class Figure {
-  // SOME COMMENT
+ private:
+  std::string name;
+ public:
+  void setName(std::string n) {
+    name = n;
+  }
+  std::string getName() {
+    return name;
+  }
 };
 
 int main() {
